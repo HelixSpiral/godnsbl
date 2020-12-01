75618479863b99feae8c42b89a3c886892393404
Shawn Smith <ShawnSmith0828@gmail.com>
2020-11-30 23:39:54 -0500

    Adding another test function

--- godnsbl_test.go
+++ godnsbl_test.go
@@ -28,3 +28,49 @@ func TestLookupIP(t *testing.T) {
 		t.Errorf("Expended a clear, got: %+v\r\n", reply)
 	}
 }
+
+func TestLookupIPGetAll(t *testing.T) {
+	dnsbl := godnsbl.NewLookupService()
+
+	dnsbl.DnsblListing = append(dnsbl.DnsblListing, godnsbl.Dnsbl{
+		Name:         "Test - DroneBL",
+		Address:      ".dnsbl.dronebl.org",
+		BlockList:    []int{1},
+		BlockMessage: "%IPADDR found",
+	})
+
+	dnsbl.DnsblListing = append(dnsbl.DnsblListing, godnsbl.Dnsbl{
+		Name:         "Test2 - DroneBL",
+		Address:      ".dnsbl.dronebl.org",
+		BlockList:    []int{1},
+		BlockMessage: "%IPADDR found",
+	})
+
+	replies := dnsbl.LookupIPGetAll("127.0.0.2")
+
+	var foundBlock bool
+	for _, reply := range replies {
+		if reply.Type == "BLOCK" {
+			foundBlock = true
+			break
+		}
+	}
+
+	if foundBlock != true {
+		t.Errorf("Did not find blocking reply: %+v\r\n", replies)
+	}
+
+	replies = dnsbl.LookupIPGetAll("2001:4860:4860::8888")
+
+	var foundClear bool
+	for _, reply := range replies {
+		if reply.Type == "CLEAR" {
+			foundClear = true
+			break
+		}
+	}
+
+	if foundClear != true {
+		t.Errorf("Did not find clearing reply: %+v\r\n", replies)
+	}
+}
